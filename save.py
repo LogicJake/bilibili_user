83bf453b70fd93502968c0b2e948ad08d9043eb3
NepoleonAsus <835410808@qq.com>
2018-02-04 18:16:13 +0800

    autocommit

--- save.py
+++ save.py
@@ -6,7 +6,7 @@ def save_user_info(datas):
     try:
         conn = pymysql.connect(host=Global.get_value('host'), user=Global.get_value('user'),
                                passwd=Global.get_value('password'), db=Global.get_value('dbname'),
-                               port=Global.get_value('port'), charset='utf8')
+                               port=Global.get_value('port'), charset='utf8',autocommit = True)
         cursor = conn.cursor()
         sql = "INSERT INTO usr_info(mid, name, approve,sex,face,regtime,place,birthday,sign) VALUES "
         for data in datas:
